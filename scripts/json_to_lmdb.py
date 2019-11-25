b68dfba6bb64d12ba48f3f04a096f397a63d6a76
zhangliang-04 <zhangliang04@hotmail.com>
2019-11-25 21:00:54 +0800

    formatted by pep8

--- scripts/json_to_lmdb.py
+++ scripts/json_to_lmdb.py
@@ -5,17 +5,18 @@ from collections import defaultdict
 import os
 import pickle
 from tqdm import tqdm
-import cv2 as cv
+
 
 def main(json_path=None, lmdb_path=None):
     assert json_path is not None, 'json_path is needed'
     if lmdb_path is None:
         lmdb_path = json_path
-    
+
     meta = os.path.join(json_path, 'meta.json')
     data_ids = []
     value = {}
-    env = lmdb.Environment(lmdb_path, subdir=True, map_size=int(1e9), max_dbs=2, lock=False)
+    env = lmdb.Environment(lmdb_path, subdir=True,
+                           map_size=int(1e9), max_dbs=2, lock=False)
     db_extra = env.open_db('extra'.encode(), create=True)
     db_image = env.open_db('image'.encode(), create=True)
     with open(meta, 'r') as meta_reader:
@@ -24,16 +25,17 @@ def main(json_path=None, lmdb_path=None):
             data_id = os.path.join(json_path, single_meta['filename'])
             data_id = str(data_id.encode('utf-8').decode('utf-8'))
             with open(data_id.encode(), 'rb') as file_reader:
-                 image = file_reader.read()
-            #value['image'] = image
+                image = file_reader.read()
             value['extra'] = {}
             for key in single_meta['extra']:
                 value['extra'][key] = single_meta['extra'][key]
             with env.begin(write=True) as lmdb_writer:
-                lmdb_writer.put(data_id.encode(), pickle.dumps(value), db=db_extra)
+                lmdb_writer.put(data_id.encode(),
+                                pickle.dumps(value), db=db_extra)
             with env.begin(write=True) as image_writer:
                 image_writer.put(data_id.encode(), image, db=db_image)
-    env.close()   
+    env.close()
+
 
 if __name__ == "__main__":
     Fire(main)
--- scripts/nori_to_lmdb.py
+++ scripts/nori_to_lmdb.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 from fire import Fire
 
 
-
 def main(nori_path, lmdb_path=None):
     if lmdb_path is None:
         lmdb_path = nori_path
-    env = lmdb.Environment(lmdb_path, map_size=int(5e10), writemap=True, max_dbs=2, lock=False)
+    env = lmdb.Environment(lmdb_path, map_size=int(
+        5e10), writemap=True, max_dbs=2, lock=False)
     fetcher = nori.Fetcher(nori_path)
     db_extra = env.open_db('extra'.encode(), create=True)
     db_image = env.open_db('image'.encode(), create=True)
@@ -25,6 +25,7 @@ def main(nori_path, lmdb_path=None):
                 writer.put(data_id.encode(), image, db=db_image)
     env.close()
     print('Finished')
+
 
 if __name__ == '__main__':
     Fire(main)
